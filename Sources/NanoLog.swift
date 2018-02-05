f3c1779383be18b24dfc4c192a787835b0e8a45b
Andrew Lord <andrewlord1990@gmail.com>
2018-02-05 21:35:47 +0000

    Setup project

---
+++ Sources/NanoLog.swift
@@ -0,0 +1,9 @@
+//
+//  NanoLog.swift
+//  NanoLog
+//
+//  Created by Andrew Lord on 05/02/2018.
+//  Copyright © 2018 NanoLog. All rights reserved.
+//
+
+import Foundation
---
+++ Tests/NanoLogTests/NanoLogTests.swift
@@ -0,0 +1,23 @@
+//
+//  NanoLogTests.swift
+//  NanoLog
+//
+//  Created by Andrew Lord on 05/02/2018.
+//  Copyright © 2018 NanoLog. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import NanoLog
+
+class NanoLogTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //// XCTAssertEqual(NanoLog().text, "Hello, World!")
+    }
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
