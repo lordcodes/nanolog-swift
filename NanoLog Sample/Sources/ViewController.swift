fde18caa5c51b0d2992dcaf0cc50ed18cbe0fe98
Andrew Lord <andrewlord1990@gmail.com>
2018-02-09 12:01:19 +0000

    #1 pretty logging (#20)
    * Setup project
    
    * Update bundle identifier
    
    * Added template for header comment
    
    * Added beginnings of NanoLog
    
    * Add log severity and methods
    
    * Fix build errors
    
    * Add sample app
    
    * Order files in projects
    
    * Log with date and tag
    
    * Give log severity a label to display
    
    * Strip parameters when printing function

---
+++ NanoLog Sample/Sources/ViewController.swift
@@ -0,0 +1,31 @@
+// Copyright (C) 2018 Andrew Lord
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+// the License.
+//
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+// an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//
+// See the License for the specific language governing permissions and limitations under the License.
+
+import NanoLog
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        NanoLog.error("Super long message that can be seen in console")
+        NanoLog.w("Super long message that can be seen in console")
+        someMethod(withIntArg: 2)
+    }
+
+    func someMethod(withIntArg intArg: Int) -> Bool {
+        NanoLog.verbose("Super long message that can be seen in console")
+        return true
+    }
+}
